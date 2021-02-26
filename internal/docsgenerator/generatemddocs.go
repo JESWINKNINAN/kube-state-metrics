189a5e3c331796461d9d240fa492847a1c3102dd
Jeswin Koshy Ninan <jeswinkninan@Jeswins-MacBook-Air.local>
2021-02-26 20:39:23 +0530

    refactor: doccollector package as per review comments

--- internal/docsgenerator/generatemddocs.go
+++ internal/docsgenerator/generatemddocs.go
@@ -15,11 +15,11 @@ limitations under the License.
 */
 package main
 
-import "k8s.io/kube-state-metrics/v2/pkg/docscollector"
+import (
+"k8s.io/kube-state-metrics/v2/pkg/docscollector")
 
 func main() {
 
-	docscollector.DocsCreate("lease-metrics")
-	docscollector.DocsCreate("resourcequota-metrics")
+docscollector.DocsCreate("configMapMetricFamilies")
 
 }
